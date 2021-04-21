a2b4a23e0628c2233277adb6e33200109a9ef6c2
김아진 <cucum.oeoe@gmail.com>
2021-04-21 23:42:38 +0900

    리스트 임시 마크업
    - 스타일 맞춰야함

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,64 @@
 <template>
-  <div>
-        select(날짜오름차순,날짜내림차순), 모두지우기, todolist list뿌려주기(삭제,완료여부체크)
-  </div>
+    <ul class="todo-list">
+        <li v-for="(item, index) of StateItems" :key="index">
+            <base-check :name="item.writeDate" :id="item.writeDate" :msg="item.content"></base-check>
+            <div class="util-wrap">
+              <button class="btn-del" @click="clearItem(item.writeDate)"></button>
+              <span class="item-date">
+                {{item.writeDate}}
+              </span>
+            </div>
+        </li>
+    </ul>
 </template>
+<script>
+import BaseCheck from '@/components/TodoCheck.vue'
+import { mapState } from 'vuex'
+
+export default {
+    name: 'TodoList',
+    components: {
+        BaseCheck,
+    },
+    computed: {
+        ...mapState({
+            StateItems: 'writeList'
+        }),
+    },
+    methods: {
+      clearItem(val) {
+        window.alert( `${val} 삭제` )
+      }
+    },
+}
+</script>
+
+<style lang="scss">
+    .todo-list {
+        li {
+            @include displayFlex($valFlexBetweenStart...);
+            position: relative;
+            background-color: $white;
+            border-radius: 5px;
+            margin-bottom: 10px;
+            @include box-shadow($defaultShadow2...);
+            padding: 1rem;
+
+            .util-wrap {
+              color: $gray9;
+              margin-left: 10px;
+            }
+        }
+        .btn-del {
+          position: relative;
+           @include icon-x();
+          // @include icon-x(10, #999, top);
+        }
+        .item-date {
+          position: absolute;
+          bottom: 0.5rem;
+          right: 1rem;
+          font-size: 0.5rem;
+        }
+    }
+</style>
