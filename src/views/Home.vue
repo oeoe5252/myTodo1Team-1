b8df459e75f2311a6db7c96bff8a4992bbd911c7
김아진_st <cucum.oeoe@gmail.com>
2021-05-22 03:41:02 +0900

    Modify 코드 가독성 향상 작업
    - 세미콜론 미사용
    - 기본 space 수정(6 to 4)
    - "," 우측 한칸 띄기
    - "연산자" 양옆 띄기
    - ":" 우측만 띄기
    - 스크립트 내 single qoutes 사용

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,24 +6,25 @@
 </template>
 
 <script>
-import DefaultLayout from "@/layouts/DefaultLayout";
-import Todo from "@/components/Todo";
-import TodoContent from "@/components/TodoContent";
-import {mapState} from 'vuex'
+import DefaultLayout from '@/layouts/DefaultLayout'
+import Todo from '@/components/Todo'
+import TodoContent from '@/components/TodoContent'
+import { mapState } from 'vuex'
+
 export default {
-      name: "App",
-      components: {
-            DefaultLayout,
-            Todo,
-            TodoContent,
-      },
-      computed : mapState({
-                  writeLists : state => state.writeList
-      }),
-      /*
-      created(){
-            this.$store.dispatch('getAllWriteList')
-      },
-      */
+    name: 'App',
+    components: {
+        DefaultLayout,
+        Todo,
+        TodoContent,
+    },
+    computed : mapState({
+        writeLists: state => state.writeList
+    }),
+    /*
+    created(){
+        this.$store.dispatch('getAllWriteList')
+    },
+    */
 };
 </script>
